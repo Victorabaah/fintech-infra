ecb4f3590e6896937176375a0584a83314415a37
Victorabaah <victor.abaah@gmail.com>
2025-05-10 19:23:36 +0100

    updated

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "class38d-terraform-backend-bucket01"
+    bucket         = "class38-terraform-backend-bucket01"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
