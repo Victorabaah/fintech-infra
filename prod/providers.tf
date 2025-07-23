4ecfe66c83ceb1ca07e8f3d47fb0a927a0aff4a1
Victorabaah <victor.abaah@gmail.com>
2025-07-23 11:20:02 +0100

    prod/providers.tf modified

--- prod/providers.tf
+++ prod/providers.tf
@@ -14,7 +14,6 @@ provider "helm" {
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
     }
   }
 }
